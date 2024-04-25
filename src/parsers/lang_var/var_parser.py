4c03257ea051d83b3d338a4b16883478e83b5fba
Janine Waidele <jwaidele@stud.hs-offenburg.de>
2024-04-25 16:49:26 +0200

    assign additon working

--- src/parsers/lang_var/var_parser.py
+++ src/parsers/lang_var/var_parser.py
@@ -2,8 +2,9 @@ from lark import ParseTree
 from parsers.lang_simple.simple_ast import *
 from parsers.common import *
 import common.log as log
+from lang_var.var_ast import *
 
-grammarFile = "./src/parsers/lang_simple/simple_grammar.lark"
+grammarFile = "./src/parsers/lang_var/var_grammar.lark"
 
 def parse(args: ParserArgs) -> exp:
     parseTree = parseAsTree(args, grammarFile, 'exp')
@@ -21,7 +22,32 @@ def parseTreeToExpAst(t: ParseTree) -> exp:
         case 'mul_exp':
             e1, e2 = [asTree(c) for c in t.children]
             return BinOp(parseTreeToExpAst(e1), Mul(), parseTreeToExpAst(e2))
-        case 'exp_1' | 'exp_2' | 'paren_exp':
+        case 'sub_exp':
+            e1, e2 = [asTree(c) for c in t.children]
+            return BinOp(parseTreeToExpAst(e1), Sub(), parseTreeToExpAst(e2))
+        case 'exp_1' | 'exp_2' | 'exp_3' | 'paren_exp':
             return parseTreeToExpAst(asTree(t.children[0]))
         case kind:
             raise Exception(f'unhandled parse tree of kind {kind} for exp: {t}')
+
+# TODO       
+def parseModule(args: ParserArgs) -> mod:
+    parseTree = parseAsTree(args, grammarFile, 'lvar')
+    return parseTreeToModuleAst(parseTree)
+
+def parseTreeToStmtAst(t: ParseTree) -> stmt:
+    match t.data:
+        case 'exp_stmt':
+            return StmtExp(parseTreeToExpAst(t))
+        case 'exp_assign':
+            return Assign(Ident(str(asToken(t.children[0]))), parseTreeToExpAst(asTree(t.children[1])))
+        case 'stmt_newline':
+            return parseTreeToStmtAst(asTree(t.children[0]))
+        case kind:
+            raise Exception(f'unhandled parse tree of kind {kind} for stmt: {t}')
+
+def parseTreeToStmtListAst(t: ParseTree) -> list[stmt]:
+    return [parseTreeToStmtAst(asTree(c)) for c in t.children]
+
+def parseTreeToModuleAst(t: ParseTree) -> mod:
+    return Module(parseTreeToStmtListAst(t))
