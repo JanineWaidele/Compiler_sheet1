f532c091e3fc3538a4516ae1c6a25fb0427239a2
Janine Waidele <jwaidele@stud.hs-offenburg.de>
2024-06-13 15:29:57 +0200

    wip

--- src/compilers/assembly/liveness.py
+++ src/compilers/assembly/liveness.py
@@ -2,6 +2,7 @@ from assembly.common import *
 from assembly.graph import Graph
 import assembly.tac_ast as tac
 from assembly.tac_ast import *
+import common.log as log
 
 def instrDef(instr: tac.instr) -> set[tac.ident]:
     """
@@ -12,13 +13,12 @@ def instrDef(instr: tac.instr) -> set[tac.ident]:
     match instr:
         case Assign(idn,_):
             defSet.add(idn)
-        case Call(_,_,args):
-            for ar in args:
-                match ar:
-                    case Name(nv):
-                        defSet.add(nv)
-                    case _:
-                        pass
+        case Call(x,_,_):
+            match x:
+                case Ident():
+                    defSet.add(x)
+                case _:
+                    pass
         case _: 
             pass
     return defSet
@@ -28,43 +28,46 @@ def instrUse(instr: tac.instr) -> set[tac.ident]:
     """
     Returns the set of identifiers used by some instrucution.
     """
-    defSet: set[tac.ident] = set()
+    useSet: set[tac.ident] = set()
     match instr:
-        case Call(cv,_,args):
-            match cv:
-                case Ident():
-                    defSet.add(cv)
-                case _:
-                    pass
+        case Call(_,_,args):
             for a in args:
                 match a:
                     case Name(v):
-                        defSet.add(v)
+                        useSet.add(v)
                     case Const():
                         pass
-        case Assign(ai, lExp):
-            defSet.add(ai)
+        case Assign(_, lExp):
             match lExp:
                 case BinOp():
                     match lExp.left:
                         case Name(vn):
-                            defSet.add(vn)
+                            useSet.add(vn)
                         case Const():
                             pass
                     match lExp.right:
                         case Name(rn):
-                            defSet.add(rn)
+                            useSet.add(rn)
                         case Const():
                             pass
                 case Prim(p):
                     match p:
                         case Name(pn):
-                            defSet.add(pn)
+                            useSet.add(pn)
                         case _:
                             pass
+
+        case GotoIf(t,_):
+            match t:
+                case Name(pn):
+                    useSet.add(pn)
+                case _:
+                    pass
+
         case _:
             pass
-    return defSet
+
+    return useSet
 
 # Each individual instruction has an identifier. This identifier is the tuple
 # (index of basic block, index of instruction inside the basic block)
@@ -97,7 +100,7 @@ class InterfGraphBuilder:
                 Lafterk = s
             else:
                 # Lafterk is the successor which was calculated in prev round
-                Lafterk = self.after[bb.index,idx_rev+1]   
+                Lafterk = self.before[bb.index,idx_rev+1]   
             # Live before are vars that weren't just defined in this instruction
             # but include the ones that have been used
             Lbeforek = Lafterk.difference(instrDef(inst)).union(instrUse(inst))
@@ -116,16 +119,19 @@ class InterfGraphBuilder:
         slide 46 here.
         """
         # Assume IN is a mapping from vertices to sets of variables.
-        # 1.Initialize IN[B] =∅ for all verticesBof the CFG.
+        # 1.Initialize IN[B] =∅ for all vertices B of the CFG.
         IN: Dict[int,Set[tac.ident]] = {x: set() for x in list(g.vertices)}
-        for b_idx in range(len(list(g.values))):
+        # 2. For each vertex B
+        # out = set of all variables present in the successors of vertex B. 
+        # Then, update the set IN[B] by taking the intersection of the IN sets of all the successors of B (IN[B']).
+        for b_idx in reversed(list(g.vertices)):
 
-            bb = list(g.values)[b_idx]
-            # TODO
-            out = IN[b_idx]
-            IN[b_idx] = self.__liveStart(bb, out)
-
-                
+            bb = g.getData(b_idx)
+            out: Set[tac.ident] = set()
+            for c in g.succs(b_idx):
+                out = out.union(IN[c])
+            self.__liveStart(bb, out)
+            IN[b_idx] = out
             
 
     def __addEdgesForInstr(self, instrId: InstrId, instr: tac.instr, interfG: InterfGraph):
@@ -136,16 +142,17 @@ class InterfGraphBuilder:
         You should implement the algorithm specified on the slide
         "Computing the interference graph" (slide 50) here.
         """
-        cfg: ControlFlowGraph = Graph('undirected')
-        self.__liveness(cfg)
         dSet = instrDef(instr)
-        uSet = instrUse(instr)
-        liveSet = dSet.union(uSet)
-        for iden in liveSet:
-            sa = self.after[instrId]
-            for aIden in sa:
-                if (iden in sa) & (iden != aIden):
-                    interfG.addEdge(iden, aIden)
+        # (x elem defSet)
+        for x in dSet:
+            # vars live after k
+            Lafterk = self.after[instrId]
+            # (y elem Lafterk)
+            for y in Lafterk:
+                # (x != y)
+                if (x != y):
+                    # all conditions True -> add edge
+                    interfG.addEdge(x, y)
 
 
     def build(self, g: ControlFlowGraph) -> InterfGraph:
@@ -160,13 +167,29 @@ class InterfGraphBuilder:
         """
         # step 1
         self.__liveness(g) # fill sets before and after
+        log.debug(f"self after: {self.after}")
+        log.debug(f"self before: {self.before}")
         # step 2
         interG: InterfGraph = Graph('undirected')
         # step 3
         for entry in g.values:
             indx = 0
             for inst in entry.instrs:
+                # add vertices for instr
+                useSet = instrUse(inst)
+                for u in useSet:
+                    if not interG.hasVertex(u):
+                        interG.addVertex(u,None)
+                defSet = instrDef(inst)
+                for d in defSet:
+                    if not interG.hasVertex(d):
+                        interG.addVertex(d,None)
+
+        for entry in g.values:
+            indx = 0
+            for inst in entry.instrs:
                 inId: InstrId = (entry.index,indx)
+                # add edges for instr to graph
                 self.__addEdgesForInstr(inId,inst,interG)
                 indx += 1
         return interG
